5fa84794f64dca54545e401b094dce673c2b0373
yjabbar <43170482+yjabbar@users.noreply.github.com>
2018-09-18 11:31:35 +0200

    Add files via upload

---
+++ untitled/untitled/src/com/company/newPlayer.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class newPlayer {
+
+    player player = new player();
+
+    {
+        player.getName();
+        player.getDefence();
+        player.getHeath();
+        player.getPower();
+    }
+}
+
+
+
+
+
+
+
+
+
