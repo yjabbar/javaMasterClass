c6fde8b10fbd782a8355ef22dd927499891d1c13
yjabbar <43170482+yjabbar@users.noreply.github.com>
2018-10-08 09:28:51 +0200

    AYZAirlines

---
+++ XYZAirport/src/main/java/com/example/XYZAirport/XyzAirportApplication.java
@@ -0,0 +1,12 @@
+package com.example.XYZAirport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzAirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzAirportApplication.class, args);
+	}
+}
---
+++ XYZAirport/src/test/java/com/example/XYZAirport/XyzAirportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.XYZAirport;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XyzAirportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
