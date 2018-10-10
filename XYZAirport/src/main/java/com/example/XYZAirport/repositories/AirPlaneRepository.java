09bd0edc037837f351b99727433b1fb4fe727eb9
yjabbar <43170482+yjabbar@users.noreply.github.com>
2018-10-10 13:55:26 +0200

    Delete AirPlaneRepository.java

--- XYZAirport/src/main/java/com/example/XYZAirport/repositories/AirPlaneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.XYZAirport.repositories;
-
-import com.example.XYZAirport.models.AirPlane;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AirPlaneRepository extends CrudRepository<AirPlane, Integer> {
-}
